28c4a93f57f8f4808afb755a8a62589d86c2a821
Boyan67 <118831149+Boyan-nolimit@users.noreply.github.com>
2023-07-24 22:15:21 +0300

    add interface and cloud functions

---
+++ functions/src/index.ts
@@ -0,0 +1,31 @@
+import * as functions from "firebase-functions";
+const admin = require("firebase-admin");
+admin.initializeApp();
+const db = admin.firestore();
+const exerciseList = require("../utils/initialExerciseList.json");
+
+export const createUserDocument = functions.auth.user().onCreate((user) => {
+  db.collection("users").doc(user.uid).set({
+    email: user.email,
+    displayName: user.displayName,
+    photoURL: user.photoURL,
+    uid: user.uid,
+  });
+
+  const batch = db.batch();
+
+  exerciseList.forEach((exercise: any) => {
+    let docRef = db
+      .collection("users")
+      .doc(user.uid)
+      .collection("exercises")
+      .doc();
+    batch.set(docRef, {
+      name: exercise.name,
+      description: exercise.description,
+      primaryMuscle: exercise.primaryMuscle,
+      secondary: exercise.secondary,
+    });
+  });
+  batch.commit();
+});
---
+++ src/app/interfaces/index.ts
@@ -0,0 +1,6 @@
+export interface Exercise {
+  name: string;
+  description: string;
+  primaryMuscle: string;
+  secondary: string;
+}
